47d3070b413975eec97b95cd0022a43e22daf41a
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-09-14 16:35:40 +0500

    customer and product tables added

--- store/models.py
+++ store/models.py
@@ -2,6 +2,31 @@ from django.db import models
 
 # Create your models here.
 
+
+
 class Product(models.Model):
+    sku = models.CharField(primary_key=True, max_length=10)
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=400)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, max_places=6)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
