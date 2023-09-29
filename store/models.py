0f82272c2dade73176bf946acee82399ac6c5350
Mehfooz <graphicx.mehfooz201@gmail.com>
2023-09-29 23:28:56 +0500

    update

--- store/models.py
+++ store/models.py
@@ -35,7 +35,7 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=11)
+    phone = models.CharField(max_length=12)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
